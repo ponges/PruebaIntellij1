be060f75112ff659c46bd57dccf8d817a7da4cfe
ponges <67662116+ponges@users.noreply.github.com>
2020-07-03 13:09:30 -0300

    Initial commit

---
+++ src/Prueba/Prueba.java
@@ -0,0 +1,18 @@
+package Prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Prueba {
+
+    public static void main( String[] args){
+
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\niced\\Documents\\Drivers\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://es.wikipedia.org");
+
+    }
+
+}
